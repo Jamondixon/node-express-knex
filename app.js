043901e8dc5eca43f12a9ac76c48698088622e5e
Jamon Dixon <jamonadixon@gmail.com>
2021-08-16 16:45:03 -0500

    migration

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/students', (request, response) => {
+    response.send({name: "Jamon"})
+})
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
